01dd236d0c38d85aa7756d6e2baa49fb0d12fb43
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-05 19:24:01 +0200

    Creación fichero librería BD y cliente de pruebas

---
+++ app/controller/DBControl.php
@@ -0,0 +1,23 @@
+<?php
+
+class DBControl{
+
+    private $user;
+    private $pwd;
+    private $dbName;
+
+    /**
+     * Constructor
+     */
+    public function __construct(){
+
+    }
+
+
+    /**
+     * Añada aquí el resto de las funciones, a conveniencia y según necesidades del software
+     */
+
+}
+
+?>
