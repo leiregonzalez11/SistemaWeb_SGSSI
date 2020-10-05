eee457b67cd9c09d5778429d6984fe1936185fb4
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-06 00:01:22 +0200

    Vistas realizadas (header, footer y pg. principal)

--- app/index.php
+++ app/index.php
@@ -1,3 +1,25 @@
-<?php
 
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="view/css/global.css">
+    <link rel="stylesheet" href="view/css/header.css">
+    <link rel="stylesheet" href="view/css/footer.css">
+    <script src="view/js/interactivo.js"></script>
+    <script src="view/js/validacion_datos.js"></script>
+    <title>Camping SGSSI</title>
+</head>
+<body>
+<?php
+    include("view/php/header.inc.php");
+
+    include("view/php/home_interface.inc.php");
+
+    include("view/php/footer.inc.php");
 ?>
+</body>
+
+</html>
---
+++ app/view/php/header.inc.php
@@ -0,0 +1,20 @@
+
+<header>
+    <nav>
+        <ul id="login_nav">
+            <li class="datos_identificacion_reg">
+                <a id="registro">REGISTRO</a>
+            </li>
+            <li class="datos_identificacion_log">
+                <a id="login">INICIAR SESIÓN</a>
+            </li>
+        </ul>
+        <ul>
+            <li><a href="">Alojamientos</a></li>
+            <li><a href="">Instalaciones</a></li>
+            <li><a href="">Galería</a></li>
+            <li><a href="">Contacto</a></li>
+            
+        </ul>
+    </nav>
+</header>
