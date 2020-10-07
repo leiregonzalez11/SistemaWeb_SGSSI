5d5e417151b5d2110132d1522429937b4922ca0a
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-07 15:03:48 +0200

    Agregado diseño Footer

---
+++ app/view/php/footer.inc.php
@@ -0,0 +1,13 @@
+<footer>
+    <div id="rrss">
+        <div id="txt_rrss">Síguenos en redes sociales:</div>
+        <div id="links_rrss">
+            <a href=""><img src="/view/img/facebook_logo.png" alt="Logotipo Facebook"></a>
+            <a href=""><img src="/view/img/twitter_logo.png" alt="Logotipo Twitter"></a>
+            <a href=""><img id="instagram_img" src="/view/img/instagram_logo.png" alt="Logotipo Instagram"></a>
+        </div>
+    </div>
+    <div id="copyright">
+        <p>Copyright &copy 2020 - SGSSI Works S.L.</p>
+    </div>
+</footer>
