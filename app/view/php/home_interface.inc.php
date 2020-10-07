7cce823131ca73bcb926b3061e233131504aa019
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-07 10:45:03 +0200

    Modificaciones semánticas en HTML y ampliaciones CSS

--- app/view/php/home_interface.inc.php
+++ app/view/php/home_interface.inc.php
@@ -7,7 +7,31 @@
         </div>
         <div id="txtWeb">
             <h1>Camping SGSSI</h1>
-            <h2>Tu camping de confianza</h2>
+            <p>Tu camping de confianza</p>
         </div>
+    </section>
+    <section id="descripciones">
+        <article>
+            <div class="descripcion">
+                <h2>Entorno agradable</h2>
+                <p>Disfrute de las vistas que hay y de la naturaleza existente en nuestro camping. En plena montaña, 
+                    desconecte de la vida de la ciudad y conecta con la libertad.
+                </p>
+            </div>
+            <div class="imagen">
+                <img src="/view/img/promo_1.jpg" alt="Paisaje de montaña"/>
+            </div>
+        </article>
+        <article>
+        <div class="descripcion">
+                <h2>Actividades infinitas</h2>
+                <p>Disfruta, con la familia o con amigos, de la realización de deportes de aventura. El personal 
+                    experto de nuestro camping os proporcionarán una experiencia inigualable e inolvidable.
+                </p>
+            </div>
+            <div class="imagen">
+                <img src="/view/img/promo_2.jpg" alt="Paisaje de montaña"/>
+            </div>
+        </article>
     </section>
 </main>
