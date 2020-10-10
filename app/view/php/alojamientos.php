5a754c9610250e43ddaef4d4260cd286cd4c42ee
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-10 16:18:34 +0200

    Desarrollo CSS sección tipos alojamientos iniciado

---
+++ app/view/php/alojamientos.php
@@ -0,0 +1,17 @@
+<main>
+    <div id="tipo_alojamiento">
+        <?php /*DEPENDIENDO DEL TIPO DE ALOJAMIENTO, 
+        AQUÍ HABRÁ UNA FOTO DISTINTA*/?>
+        <div id="container_foto">
+            <img src="/view/img/alojamiento_bungalow.jpg">
+        </div>
+        <div id="container_texto">
+            <h1>[TIPO_ALOJAMIENTO AQUÍ]</h1>
+            <p>[DESCRIPCIÓN_BREVE_POMPOSA_AQUÍ]</p>
+        </div>
+    </div>
+    <section id="listado_alojamientos">
+    <h1>Hola</h1>
+    </section>
+    
+</main>
