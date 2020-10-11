0243c7e97579a2317c1ab3b222a91011bd072537
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-11 14:49:18 +0200

    CSS Listado alojamientos finalizado

--- app/view/php/alojamientos.php
+++ app/view/php/alojamientos.php
@@ -11,7 +11,35 @@
         </div>
     </div>
     <section id="listado_alojamientos">
-    <h1>Hola</h1>
+        <a href="[ENLACE_ALOJAMIENTO]">
+            <div class="elem_alojamiento">
+                <div class="container_foto_listado">
+                    <img src="/view/img/alojamiento_bungalow.jpg"/>
+                </div>
+                <div class="txt_listado">
+                    <p>Tipo: [TIPO]</p>
+                    <p>Metros cuadrados: [METROS_CUADRADOS]</p>
+                </div>
+                <div class="accion_listado">
+                    <p>&gt;</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="[ENLACE_ALOJAMIENTO]">
+            <div class="elem_alojamiento">
+                <div class="container_foto_listado">
+                    <img src="/view/img/alojamiento_bungalow.jpg"/>
+                </div>
+                <div class="txt_listado">
+                    <p>Tipo: [TIPO]</p>
+                    <p>Metros cuadrados: [METROS_CUADRADOS]</p>
+                </div>
+                <div class="accion_listado">
+                    <p>&gt;</p>
+                </div>
+            </div>
+        </a>
     </section>
     
 </main>
