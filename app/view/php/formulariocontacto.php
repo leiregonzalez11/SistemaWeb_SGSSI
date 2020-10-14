1eace6bbb4a7ba9d76a30fc4ccb26618c7cb5dcd
Leire <leiregonzalezlopez992gmail.com>
2020-10-14 17:17:50 +0200

    Varias cosas añadidas/creadas (ver descr.)
    -Creadas quienessomos, instalaciones, donde estamos , formulario de contacto, etc.
    - Filosofia finalizada, queda el css
    -Puesto un icono en la pagina

---
+++ app/view/php/formulariocontacto.php
@@ -0,0 +1,31 @@
+<form action="contacto.php" method="post">
+  <div class="elem-group">
+    <label for="name">Esriba aqui su nombre</label>
+    <input type="text" id="name" name="visitor_name" placeholder="Maripili Gonzalez" pattern=[A-Z\sa-z]{3,20} required>
+  </div>
+  <div class="elem-group">
+    <label for="email">Escribe aqui tu email</label>
+    <input type="email" id="email" name="visitor_email" placeholder="mail@mail.com" required>
+  </div>
+  <div class="elem-group">
+    <label for="seleccion-motivo">Elija el motivo de su consulta</label>
+    <select id="seleccion-motivo" name="motivo-consulta" required>
+        <option value="">Seleccione un motivo</option>
+        <option value="queja">Queja</option>
+        <option value="reserva">Reserva de alojamiento</option>
+        <option value="masinfo">Más información</option>
+        <option value="otro">Otro Motivo</option>
+    </select>
+  </div>
+  <div class="elem-group">
+    <label for="title">Escriba aqui el asunto del mensaje</label>
+    <input type="text" id="title" name="email_title" required placeholder="No puedo restablecer mi contraseña" pattern=[A-Za-z0-9\s]{8,60}>
+  </div>
+  <div class="elem-group">
+    <label for="message">Escribe aqui tu mensaje</label>
+    <textarea id="message" name="visitor_message" placeholder="Escriba aqui su mensaje" required></textarea>
+  </div>
+  <button type="submit">Enviar Mensaje</button>
+</form>
+
+
