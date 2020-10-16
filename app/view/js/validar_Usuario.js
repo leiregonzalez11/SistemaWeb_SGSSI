f46888737e8bca7f8522f1b33b160c32b0224a2d
Leire <leiregonzalezlopez992gmail.com>
2020-10-17 01:12:05 +0200

    validar mail 3

--- app/view/js/validar_Usuario.js
+++ app/view/js/validar_Usuario.js
@@ -82,7 +82,7 @@ function dniCorrecto(){
   function correoCorrecto(){
     var email = document.getElementById("mail_reg");
     var data = email.value;
-    var exp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/
+    var exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if(!exp.test(data)){
       email.className='fallo';
       return false;
