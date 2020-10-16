65c026b994c9334f7ed8d48a8331905090f6da41
unai <unai13hego@gmail.com>
2020-10-16 22:23:08 +0200

    Update quienessomos.php

--- app/view/php/quienessomos.php
+++ app/view/php/quienessomos.php
@@ -1,6 +1,16 @@
 <main>
   <h1>¿Quienes somos?</h1>
   <div id="contenedor_texto">
-     <p> Hola</p>
+     <p> 
+       Somos un equipo de personas que quieren ofrecer la posibilidad de descanso y relax a todos sus clientes.
+       También queremos ofrecer el que tanto niños como adultos tengan una estancia satisfactoria y llena de 
+       entretenimiento.
+     </p>
+     <p>
+       Nuestro equipo se compone de personas que siempre están a disposición de los clientes y dispuestos a 
+       solucionar cualquier problema que estos tengan durante su estancia en nuestro cámping. Es geste comprometida
+       con su trabajo y en el caso de los encargados de las instalaciones infantiles además son personas con 
+       experiencia en el área infantil. 
+     </p>
   </div>
 </main>
