39f6c191f5129103b642caf26df2e0d403480033
unai <unai13hego@gmail.com>
2020-10-17 17:33:13 +0200

    Update instalaciones.php

--- app/view/php/instalaciones.php
+++ app/view/php/instalaciones.php
@@ -1,6 +1,33 @@
 <main>
   <h1>Instalaciones</h1>
   <div id="contenedor_texto">
-    <p> Hola </p>
+    <h2>Instalaciones infantiles</h2>
+    <p>
+      En nuestro camping disponemos de instalaciones para los más pequeños. Tanto parque infantil para todos 
+      los días del año como un parque infantil acuático para los días más calurosos del verano.
+    </p>
+    <h2>Instalaciones de ocio</h2>
+    <p>
+      Disponemos por ejemplo de una ludoteca para principalmente los más pequeños. Aunque estas instalaciones 
+      también lo pueden usar los adultos. Tambien disponemos de una terraza en el bar en el que se puede pasar 
+      el rato en todo momento. Por supuesto, se podrá ir a la recepción en todo momento y se podrá pasar todo 
+      el tiempo que se quiera en ésta.
+    </p>
+    <h2>Bar/Restaurante</h2>
+    <p>
+      También disponemos de un restaurante en el que se sirve desayuno, comida y cena. En el desayuno habrá 
+      varias opciones para que se pueda hacer un desayuno más al gusto del consumidor. En la comida y cena se 
+      podrá elegir entre menú del día o diversos platos combinados y entrantes. Además disponemos de menús 
+      infantiles pues, somos conscientes que hay edades en las que es más difícil que se coma de todo. Así 
+      podrán todos comer a gusto.
+    </p>
+    <h2>Piscina y spa</h2>
+    <p>
+      Disponemos además de una piscina vigilada entre las 08:00 de la mañana y las 20:00 de la noche por un 
+      socorrista. Por esto, se podrá hacer uso de esta en cualquier momento dentro del horario. El spa estará 
+      abierto en el mismo horario que la piscina. Dentro del spa disponemos entre otros elementos de un jacuzzi 
+      y una sauna. También ofrecemos servicios estéticos e incluso de servicio de masajista. Ambos estarán 
+      disponibles a diario desde las 10:00 de la mañana y las 18:00 de la tarde.
+    </p>
   </div>
 </main>
