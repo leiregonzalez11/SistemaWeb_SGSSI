a2c52132a981aa0d2aa868a3c6af088db8394ae9
Christian B <cberrocal001@ikasle.ehu.eus>
2020-10-18 12:15:06 +0200

    Más reformas CSS

--- app/view/php/instalaciones.php
+++ app/view/php/instalaciones.php
@@ -3,47 +3,70 @@
   <div id="contenedor_texto">
     <h2>Instalaciones infantiles</h2>
     <p>
-      En nuestro camping disponemos de instalaciones para los más pequeños. Tanto parque infantil para todos 
+      En nuestro camping disponemos de instalaciones para los más pequeños. Tanto parque infantil para todos
       los días del año como una piscina infantil acuático para los días más calurosos del verano.
     </p>
     <div class="imagenes">
-      <img src="/view/img/fotosinstalaciones/parqueinfantil.jpg" alt="Parque infantil"/>
-      <img src="/view/img/fotosinstalaciones/piscinainfantil.jpg" alt="Piscina infantil"/>
+      <div class="container_img">
+        <img src="/view/img/fotosinstalaciones/parqueinfantil.jpg" alt="Parque infantil" />
+      </div>
+      <div class="container_img">
+        <img src="/view/img/fotosinstalaciones/piscinainfantil.jpg" alt="Piscina infantil" />
+      </div>
+
     </div>
     <h2>Instalaciones de ocio</h2>
     <p>
-      Disponemos por ejemplo de una ludoteca para principalmente los más pequeños. Aunque estas instalaciones 
-      también lo pueden usar los adultos. Tambien disponemos de una terraza en el bar en el que se puede pasar 
-      el rato en todo momento. Por supuesto, se podrá ir a la recepción en todo momento y se podrá pasar todo 
+      Disponemos por ejemplo de una ludoteca para principalmente los más pequeños. Aunque estas instalaciones
+      también lo pueden usar los adultos. Tambien disponemos de una terraza en el bar en el que se puede pasar
+      el rato en todo momento. Por supuesto, se podrá ir a la recepción en todo momento y se podrá pasar todo
       el tiempo que se quiera en ésta.
     </p>
     <div class="imagenes">
-      <img src="/view/img/fotosinstalaciones/ludoteca.jpg" alt="Ludoteca"/>
-      <img src="/view/img/fotosinstalaciones/barchillout.jpg" alt="Bar chill out"/>
-      <img src="/view/img/fotosinstalaciones/recepcion.jpg" alt="Recepción"/>
+      <div class="container_img">
+        <img src="/view/img/fotosinstalaciones/ludoteca.jpg" alt="Ludoteca" />
+      </div>
+      <div class="container_img">
+        <img src="/view/img/fotosinstalaciones/barchillout.jpg" alt="Bar chill out" />
+      </div>
+      <div class="container_img">
+        <img src="/view/img/fotosinstalaciones/recepcion.jpg" alt="Recepción" />
+      </div>
+
+
+
     </div>
     <h2>Bar/Restaurante</h2>
     <p>
-      También disponemos de un restaurante en el que se sirve desayuno, comida y cena. En el desayuno habrá 
-      varias opciones para que se pueda hacer un desayuno más al gusto del consumidor. En la comida y cena se 
-      podrá elegir entre menú del día o diversos platos combinados y entrantes. Además disponemos de menús 
-      infantiles pues, somos conscientes que hay edades en las que es más difícil que se coma de todo. Así 
+      También disponemos de un restaurante en el que se sirve desayuno, comida y cena. En el desayuno habrá
+      varias opciones para que se pueda hacer un desayuno más al gusto del consumidor. En la comida y cena se
+      podrá elegir entre menú del día o diversos platos combinados y entrantes. Además disponemos de menús
+      infantiles pues, somos conscientes que hay edades en las que es más difícil que se coma de todo. Así
       podrán todos comer a gusto.
     </p>
     <div class="imagenes">
-      <img src="/view/img/fotosinstalaciones/restaurante.jpg" alt="Restaurante del camping"/>
+      <div class="container_img">
+      <img src="/view/img/fotosinstalaciones/restaurante.jpg" alt="Restaurante del camping" />
+      </div>
+      
     </div>
     <h2>Piscina y spa</h2>
     <p>
-      Disponemos además de una piscina vigilada entre las 08:00 de la mañana y las 20:00 de la noche por un 
-      socorrista. Por esto, se podrá hacer uso de esta en cualquier momento dentro del horario. El spa estará 
-      abierto en el mismo horario que la piscina. Dentro del spa disponemos entre otros elementos de un jacuzzi 
-      y una sauna. También ofrecemos servicios estéticos e incluso de servicio de masajista. Ambos estarán 
+      Disponemos además de una piscina vigilada entre las 08:00 de la mañana y las 20:00 de la noche por un
+      socorrista. Por esto, se podrá hacer uso de esta en cualquier momento dentro del horario. El spa estará
+      abierto en el mismo horario que la piscina. Dentro del spa disponemos entre otros elementos de un jacuzzi
+      y una sauna. También ofrecemos servicios estéticos e incluso de servicio de masajista. Ambos estarán
       disponibles a diario desde las 10:00 de la mañana y las 18:00 de la tarde.
     </p>
     <div class="imagenes">
-      <img src="/view/img/fotosinstalaciones/piscina.jpg" alt="Piscina"/>
-      <img src="/view/img/fotosinstalaciones/spa.jpg" alt="Spa"/>
+      <div class="container_img">
+      <img src="/view/img/fotosinstalaciones/piscina.jpg" alt="Piscina" />
+      </div>
+      <div class="container_img">
+      <img src="/view/img/fotosinstalaciones/spa.jpg" alt="Spa" />
+      </div>
+      
+      
     </div>
   </div>
 </main>
