9f05d489ce1aeef2d771aa6d2b80eeeb03b28f64
unai <unai13hego@gmail.com>
2020-10-18 11:39:07 +0200

    Fotos de instalaciones

--- app/view/php/instalaciones.php
+++ app/view/php/instalaciones.php
@@ -4,8 +4,12 @@
     <h2>Instalaciones infantiles</h2>
     <p>
       En nuestro camping disponemos de instalaciones para los más pequeños. Tanto parque infantil para todos 
-      los días del año como un parque infantil acuático para los días más calurosos del verano.
+      los días del año como una piscina infantil acuático para los días más calurosos del verano.
     </p>
+    <div class="imagen">
+      <img src="/view/img/fotosinstalaciones/parqueinfantil.jpg" alt="Parque infantil"/>
+      <img src="/view/img/fotosinstalaciones/piscinainfantil.jpg" alt="Piscina infantil"/>
+    </div>
     <h2>Instalaciones de ocio</h2>
     <p>
       Disponemos por ejemplo de una ludoteca para principalmente los más pequeños. Aunque estas instalaciones 
@@ -13,6 +17,11 @@
       el rato en todo momento. Por supuesto, se podrá ir a la recepción en todo momento y se podrá pasar todo 
       el tiempo que se quiera en ésta.
     </p>
+    <div class="imagen">
+      <img src="/view/img/fotosinstalaciones/ludoteca.jpg" alt="Ludoteca"/>
+      <img src="/view/img/fotosinstalaciones/bar chill out.jpg" alt="Bar chill out"/>
+      <img src="/view/img/fotosinstalaciones/recepcion.jpg" alt="Recepción"/>
+    </div>
     <h2>Bar/Restaurante</h2>
     <p>
       También disponemos de un restaurante en el que se sirve desayuno, comida y cena. En el desayuno habrá 
@@ -21,6 +30,9 @@
       infantiles pues, somos conscientes que hay edades en las que es más difícil que se coma de todo. Así 
       podrán todos comer a gusto.
     </p>
+    <div class="imagen">
+      <img src="/view/img/fotosinstalaciones/restaurante.jpg" alt="Restaurante del camping"/>
+    </div>
     <h2>Piscina y spa</h2>
     <p>
       Disponemos además de una piscina vigilada entre las 08:00 de la mañana y las 20:00 de la noche por un 
@@ -29,5 +41,9 @@
       y una sauna. También ofrecemos servicios estéticos e incluso de servicio de masajista. Ambos estarán 
       disponibles a diario desde las 10:00 de la mañana y las 18:00 de la tarde.
     </p>
+    <div class="imagen">
+      <img src="/view/img/fotosinstalaciones/piscina.jpg" alt="Piscina"/>
+      <img src="/view/img/fotosinstalaciones/spa.jpg" alt="Spa"/>
+    </div>
   </div>
 </main>
