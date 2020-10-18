8883f0ce76f0a7135885631e2d7e30cd8fbdc738
unai <unai13hego@gmail.com>
2020-10-18 12:05:10 +0200

    localizacion

--- app/view/php/dondeestamos.php
+++ app/view/php/dondeestamos.php
@@ -3,6 +3,9 @@
   <h1>¿Dónde Estamos?</h1>
   <div id="contenedor_texto">
     <h2>Localización</h2>
-     <p> Hola</p>
+     <p> Nuestro camping se encuentra en la Cornisa Cantábrica. Más concretamente en la población de Riba de Sella Asturias.</p>
   </div>
+  <div class="imagen">
+      <img src="/view/img/Localizacion.jpg" alt="Localizacion"/>
+    </div>
 </main>
