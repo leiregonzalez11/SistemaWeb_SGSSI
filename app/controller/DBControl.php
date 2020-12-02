440eb289b070922cdbd2f89f99283a30cbb0c6e6
Christian B <cberrocal001@ikasle.ehu.eus>
2020-12-02 01:53:29 +0100

    Arreglando pequeño error

--- app/controller/DBControl.php
+++ app/controller/DBControl.php
@@ -228,10 +228,10 @@ class DBControl{
         $idA=mysqli_insert_id($enlace);
         $existe=mysqli_query($enlace,"SELECT EXISTS (SELECT * FROM Alojamiento WHERE idAlojamiento='$idA');");
         $reg=mysqli_num_rows($existe);
-        mysqli_close ($enlace);
+        //mysqli_close ($enlace);
 
         $stmt=$enlace->prepare("INSERT INTO Alojamiento (descripcion, metrosCuadrados, capacidad, tipo) VALUES (?,?,?,?)");
-        $stmt->bind_param("ssss",$descr, $m2, $cap, $tipo);
+        $stmt->bind_param("sdis",$descr, $m2, $cap, $tipo);
         $stmt->execute();
         $stmt->close();
         $idA=mysqli_insert_id($enlace);
